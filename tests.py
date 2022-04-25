7c211611bfcaa2ea6c9ce172b4d353c479d71224
Katya <prokhorchuk.e@phystech.edu>
2022-04-25 15:00:38 +0300

    fixed tests.py

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
-from shell import *
+import os
+import shell
 import unittest
 
 
@@ -7,19 +8,19 @@ class TestBash(unittest.TestCase):
 
     def test_cd(self):
         test = ['cd', 'aa/gg/hh']
-        self.assertEqual(2, def_cd(test))
+        self.assertEqual(2, shell.def_cd(test))
 
     def test_dir(self):
         os.mkdir('testdir')
         test = ['rmdir', 'testdir']
-        self.assertEqual(1, del_dir(test))
-        self.assertEqual(2, del_dir(test))
+        self.assertEqual(1, shell.del_dir(test))
+        self.assertEqual(2, shell.del_dir(test))
 
     def test_make_dir(self):
         # os.mkdir('testdir')
         test = ['rmdir', 'testdir']
-        self.assertEqual(1, make_folder(test))
-        self.assertEqual(2, make_folder(test))
+        self.assertEqual(1, shell.make_folder(test))
+        self.assertEqual(2, shell.make_folder(test))
         os.rmdir('testdir')
 
     def test_copy(self):
@@ -31,27 +32,27 @@ class TestBash(unittest.TestCase):
         file = open('test1.txt', "a")
         file.write("hello world")
         file.close()
-        self.assertEqual(1, copy(test))
+        self.assertEqual(1, shell.copy(test))
         os.remove('test.txt')
         os.remove('test1.txt')
         test = ['cp', 'test.txt']
-        self.assertEqual(2, copy(test))
+        self.assertEqual(2, shell.copy(test))
 
     def test_move(self):
         test = ['mv', 'test.txt']
-        self.assertEqual(2, move(test))
+        self.assertEqual(2, shell.move(test))
         os.mkdir('testdir')
         test = ['mv', 'test.txt', 'testdir/']
-        self.assertEqual(2, move(test))
+        self.assertEqual(2, shell.move(test))
         os.rmdir('testdir')
 
     def test_execute(self):
-        self.assertEqual(1, execute('pwd'))
+        self.assertEqual(1, shell.execute('pwd'))
 
     def test_delete(self):
         file = open('file.txt', 'a')
         file.close()
         test = ['rm', 'test.txt']
-        self.assertEqual(2, delete(test))
+        self.assertEqual(2, shell.delete(test))
         test = ['rm', 'file.txt']
-        self.assertEqual(1, delete(test))
+        self.assertEqual(1, shell.delete(test))
